51724b7257217cb01a3b55e45120f6e9f1d82d1d
omar0ali <bajunaidomar@gmail.com>
2025-07-10 09:56:58 -0400

    Adding player, and testing other attributes (speed, framerate) ..etc

--- main.go
+++ main.go
@@ -6,21 +6,32 @@ import (
 	"github.com/gdamore/tcell/v2"
 )
 
+var player *Player
+
 func main() {
-	window, err := CreateWindow("Breakout Game", 33)
+	window, err := CreateWindow("Breakout Game", 25)
 	if err != nil {
 		log.Panic(err)
 	}
 	exit := make(chan int)
+
+	// Objects
+	player := CreatePlayer(10, window)
+
 	window.InitEventsKeys(
 		func(ek *tcell.EventKey) {
 			switch ek.Key() {
 			// to update an object coordiatnes, not to animate
 			case tcell.KeyUp:
 				window.SetContent(0, 0, 'O')
+			case tcell.KeyLeft:
+				player.UpdateCoords(-2) // left
+			case tcell.KeyRight:
+				player.UpdateCoords(2) // right
 			}
 		}, func() {
 			// animation to draw
+			player.Update()
 		}, exit,
 	)
 
