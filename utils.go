ea685f3f47826809966f73b1ff35d7511307aa01
omar0ali <bajunaidomar@gmail.com>
2025-07-11 19:33:21 -0400

    Minor helpful objects for cleaner code

---
+++ utils.go
@@ -0,0 +1,18 @@
+package main
+
+type Point struct {
+	X float64
+	Y float64
+}
+
+type Direction struct {
+	Up    bool
+	Down  bool
+	Left  bool
+	Right bool
+}
+
+type Velocity struct {
+	X float64
+	Y float64
+}
