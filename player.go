5badbbf9560cd033ac5acf4c53ed4f60b5abf9ba
omar0ali <bajunaidomar@gmail.com>
2025-07-12 12:24:26 -0400

    Player update, sitll need more cleaning :(

--- player.go
+++ player.go
@@ -3,38 +3,40 @@ package main
 import "github.com/gdamore/tcell/v2"
 
 type Player struct {
-	X      int
-	width  int
-	window *Window
+	X           float64
+	PaddleWdith int
+	Direction   Direction
 }
 
-func CreatePlayer(width int, window *Window) *Player {
+const PlayerSpeed float64 = 10
+
+func CreatePlayer(paddleWdith int) *Player {
 	screenWidth, _ := window.GetScreenSize()
-	middlePos := (screenWidth / 2) - (width / 2) // the middle pos is that start drawing the the bar
+	middlePos := float64((screenWidth / 2) - (paddleWdith / 2)) // the middle pos is that start drawing the the bar
 
 	return &Player{
-		X:      middlePos,
-		width:  width,
-		window: window,
+		X:           middlePos,
+		PaddleWdith: paddleWdith,
 	}
 }
 
 func (p *Player) Draw() {
-	_, height := p.window.GetScreenSize()
-	for i := 0; i < p.width; i++ {
-		p.window.SetContent(p.X+i, height-1, tcell.RuneBlock)
+	_, height := window.GetScreenSize()
+	for i := 0; i < p.PaddleWdith; i++ {
+		window.SetContent(int(p.X+float64(i)), height-1, tcell.RuneBlock)
 	}
 }
 
 // this function is used to update the coordinates of the player, so they can move left or right
 
-func (p *Player) UpdateCoords(x int) {
-	width, _ := p.window.GetScreenSize()
-	if p.X+p.width >= width-1 {
-		p.X = p.X - 1
+func (p *Player) UpdateCoords(x float64) {
+	width, _ := window.GetScreenSize()
+
+	if p.X+float64(p.PaddleWdith) > float64(width) {
+		p.X = float64(width - p.PaddleWdith)
 		return
-	} else if p.X <= 1 {
-		p.X = p.X + 1
+	} else if p.X < 0 {
+		p.X = 0
 		return
 	}
 	p.X = p.X + x
