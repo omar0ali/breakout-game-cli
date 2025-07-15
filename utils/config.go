75295e961a369e98169dbd90c162c2653b301f2d
omar0ali <bajunaidomar@gmail.com>
2025-07-15 13:28:50 -0400

    The configuration/Config Object. And decoding it with a BruntSushi/toml

---
+++ utils/config.go
@@ -0,0 +1,23 @@
+package utils
+
+import "github.com/BurntSushi/toml"
+
+type Config struct {
+	Player struct {
+		Speed       float64 `toml:"speed"`
+		JumpBy      float64 `toml:"jump_by"`
+		PaddleWdith float64 `toml:"paddle_wdith"`
+	} `toml:"player"`
+
+	Ball struct {
+		Speed float64 `toml:"speed"`
+	} `toml:"ball"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	var config Config
+	if _, err := toml.DecodeFile(path, &config); err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
